87f35235ee75eb22c5642f7c60356d36c3653e80
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-29 17:44:54 +0300

    :sparkles: Update hero section

--- components/home/hero-section/index.jsx
+++ components/home/hero-section/index.jsx
@@ -7,7 +7,7 @@ import Image from '../../../assets/undraw_Programming_re_kg9v.svg'
 const HeroSection = () => {
   console.log(Image)
   return (
-    <div className="section grid grid-cols-1 bg-[#01014a] dark:bg-transparent md:max-h-[600px] md:grid-cols-2">
+    <div className="section grid grid-cols-1 bg-[#01014a] md:max-h-[600px] md:grid-cols-2">
       <div
         style={{
           backgroundImage: `url(${Image.src})`,
