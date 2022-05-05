d6ec42c4282c9d2ac4a57abb7f17eabcf5cb58bd
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-05 22:08:24 +0300

    :sparkles: Footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,22 +5,82 @@ export const Footer = () => {
   return (
     <div className="bg-dim-dark">
       <div className="container min-h-[200px] py-6 text-white">
-        <h4>Footer</h4>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div>
+            <h4 className="text-lg">Positions Of Interest</h4>
+            <div className="my-2 h-[1px] w-16 bg-white bg-opacity-40"></div>
+
+            <div className="mt-6 flex flex-wrap gap-2 text-sm">
+              <div className="cursor-pointer rounded-xl bg-white bg-opacity-5 py-2 px-5 hover:bg-opacity-20">
+                React Js Developer
+              </div>
+              <div className="cursor-pointer rounded-xl bg-white bg-opacity-5 py-2 px-5 hover:bg-opacity-20">
+                Node Js Developer
+              </div>
+              <div className="cursor-pointer rounded-xl bg-white bg-opacity-5 py-2 px-5 hover:bg-opacity-20">
+                Angular Developer
+              </div>
+              <div className="cursor-pointer rounded-xl bg-white bg-opacity-5 py-2 px-5 hover:bg-opacity-20">
+                Full Stack Developer
+              </div>
+              <div className="cursor-pointer rounded-xl bg-white bg-opacity-5 py-2 px-5 hover:bg-opacity-20">
+                Lead Web Developer
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h4 className="text-lg">Salary Range</h4>
+            <div className="my-2 h-[1px] w-16 bg-white bg-opacity-40"></div>
+
+            <div className="mt-6">
+              <h2>Annual</h2>
+              <p className="my-2 flex items-center gap-2">
+                <span className="text-sm text-white opacity-40">From</span>
+                <span>93,000 $USD</span>
+                <span className="text-sm text-white opacity-40">To</span>
+                <span>+250,000 $USD</span>
+              </p>
+              <h2>Per hour:</h2>
+              <p className="my-2 flex items-center gap-2">
+                <span className="text-sm text-white opacity-40">From</span>
+                <span>20.00 $USD</span>
+                <span className="text-sm text-white opacity-40">To</span>
+                <span>+100.22 $USD</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white bg-opacity-10 p-4 rounded-xl">
+            <p className='text-xs tracking-wide'>
+              NOTE: In case you have an idea for a project but you estimate that
+              your budget may be very small, do not refrain from contacting me
+              and presenting your project to me. I'm working to provide flexible
+              payment methods that allow us to carry out great projects, without
+              cost being a barrier.
+            </p>
+
+            <button className="w-full mt-5 rounded-full font-semibold bg-white py-2 text-center text-dark">
+              Schedule a meeting
+            </button>
+          </div>
+        </div>
       </div>
       <div className="bg-white py-10">
         <div className="container flex flex-col items-start gap-4 md:flex-row md:justify-center">
-          <div className="flex flex-col flex-1">
+          <div className="flex flex-1 flex-col">
             <p>
               This website has been designed and developed by me from scratch :)
             </p>
             <p className="text-sm">
-              Copyright <span className="text-primary font-bold">@Martin</span> {new Date().getFullYear()}
+              Copyright <span className="font-bold text-primary">@Martin</span>{' '}
+              {new Date().getFullYear()}
             </p>
           </div>
           <Link href="mailto:mwangimartin1904@gmail.com">
             <a
               target="_blank"
-              className="rounded-full bg-primary py-2 px-8  text-white text-center inline-block"
+              className="inline-block rounded-full bg-primary py-2  px-8 text-center text-white"
             >
               Contact Me
             </a>
