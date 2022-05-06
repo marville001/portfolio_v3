c1a4e1816dbce6f21b5e5dc78df4c002f1d9bc63
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-06 14:17:44 +0300

    Update project schema

---
+++ studio/schemas/project.js
@@ -0,0 +1,52 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'github',
+      title: 'Github',
+      type: 'url',
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'url',
+    },
+
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+  ],
+}
