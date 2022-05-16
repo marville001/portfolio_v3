5f6b9d5ad3f56ba2d255a60b4eca0fd401e3c2f9
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-16 22:01:21 +0300

    :sparkles: update tools

--- components/home/lang-tools-section/index.tsx
+++ components/home/lang-tools-section/index.tsx
@@ -23,19 +23,31 @@ const langs = [
 
 const LangToolsSection = () => {
   return (
-    <div className="section  dark:text-white bg-slate-50 py-10 dark:bg-dark">
-      <div className="container">
-        <h2 className="text-center dark:text-white text-3xl font-bold text-dim-dark">
-          Programming Languages and Tools
-        </h2>
+    <div
+      style={{
+        backgroundImage:
+          'url(/assets/gradient-islamic-pattern-background-vector.jpg)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="section  bg-slate-50 py-10 dark:bg-dark dark:text-white bg-opacity-60">
+        <div className="container">
+          <h2 className="text-center text-3xl font-bold text-dim-dark dark:text-white">
+            Programming Languages and Tools
+          </h2>
 
-        <div className="flex justify-center gap-5 my-10 flex-wrap">
-          {langs.map((lang) => (
-            <div key={lang.id} className='hover:shadow p-3 cursor-pointer px-5'>
-              <img className="w-14" src={lang.icon} alt="" />
-              <p className='mt-3 text-sm text-center'>{lang.name}</p>
-            </div>
-          ))}
+          <div className="my-10 flex flex-wrap justify-center gap-5">
+            {langs.map((lang) => (
+              <div
+                key={lang.id}
+                className="cursor-pointer flex flex-col transition-all duration-100 ease-linear items-center p-3 px-5 hover:shadow bg-opacity-100 hover:bg-white rounded-lg"
+              >
+                <img className="w-14" src={lang.icon} alt="" />
+                <p className="mt-3 text-center text-sm">{lang.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
