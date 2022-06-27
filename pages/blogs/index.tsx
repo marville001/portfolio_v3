482a4af351351298b53948c9432aad3a834c8d73
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-28 00:04:26 +0300

    Update blogs page

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 import ContainerBlock from '../../components/ContainerBlock'
 
 const Blogs: NextPage = () => {
@@ -12,9 +13,10 @@ const Blogs: NextPage = () => {
         <div className="md-px-6 mx-auto min-h-[60vh] max-w-[900px] py-10 px-2 sm:px-4">
           <form>
             <div className="relative">
+              <FaSearch className="absolute left-4 top-1/2 -translate-y-1/2 peer-focus:text-red-400" />
               <input
                 type="search"
-                className="w-full rounded border px-4 py-2 text-lg focus:outline-none focus:ring-1"
+                className="w-full rounded border peer bg-primary bg-opacity-10 px-4 pl-12 py-2 text-lg focus:bg-white focus:outline-none focus:ring-1"
                 placeholder="Search my blogs here..."
               />
             </div>
