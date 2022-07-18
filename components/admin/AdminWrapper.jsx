58f99ca9c4e9b852ec00768425349717f9e7d9ac
Martin Mwangi <mwangimartin1904@gmail.com>
2022-07-19 00:03:40 +0300

    Protect route

--- components/admin/AdminWrapper.jsx
+++ components/admin/AdminWrapper.jsx
@@ -1,4 +1,17 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useAuth } from '../../contexts/auth.context'
+
 const AdminWrapper = ({ children }) => {
+  const authContext = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!authContext.user?.uid) {
+      router.push('/')
+    }
+  }, [authContext.user])
+
   return (
     <div>
       <div className="min-h-[800px] py-5 px-4">{children}</div>
