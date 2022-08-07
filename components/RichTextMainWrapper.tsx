e7b9e264513690998375a6287e7570ee5d76fab5
Martin Mwangi <mwangimartin1904@gmail.com>
2022-08-07 21:17:58 +0300

    Updates on blog reading

--- components/RichTextMainWrapper.tsx
+++ components/RichTextMainWrapper.tsx
@@ -2,17 +2,19 @@ import React, { ReactChild, ReactChildren } from 'react'
 
 const RichTextMainWrapper = ({ children }: { children: ReactChild }) => {
   return (
-    <div className='flex-1
-                prose prose-slate lg:prose-lg mt-6 
+    <div className='
+                prose prose-slate lg:prose-lg mt-6
                 prose-p:text-[16px] lg:prose-p:text-[17px]   prose-p:leading-8 
-                prose-pre:bg-dim-dark prose-pre:text-[10px] sm:prose-pre:text-[12px] lg:prose-pre:text-[16px] prose-pre:w-[100%] prose-pre:aspect-auto
+                prose-pre:bg-dim-dark prose-pre:text-[10px] sm:prose-pre:text-[12px] 
+                lg:prose-pre:text-[16px] prose-pre:whitespace-pre
                 prose-a:!text-primary prose-a:underline
                 prose-h1:my-6 lg:prose-h1:my-8 prose-h2:my-5 lg:prose-h2:my-7
                 prose-h3:my-4 lg:prose-h3:my-6 prose-h4:my-4 lg:prose-h4:my-6
                 prose-h5:my-3 lg:prose-h5:my-4 prose-h6:my-3 lg:prose-h6:my-4
-                prose-ul:leading-8 prose-ul:text-[16px] 
-                prose-ol:leading-8 prose-ol:text-[16px]
-                '>{children}</div>
+                prose-ul:leading-s8 prose-ul:text-[16px] prose-ul:w-[90%]
+                prose-ol:leading-s8 prose-ol:text-[16px]
+      '
+    >{children}</div>
   )
 }
 
