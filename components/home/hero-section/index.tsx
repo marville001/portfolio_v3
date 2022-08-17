87eac7b0c28102cb943b9d6f1be28484670abfb3
Martin Mwangi <mwangimartin1904@gmail.com>
2022-08-17 22:13:16 +0300

    Update hero section

--- components/home/hero-section/index.tsx
+++ components/home/hero-section/index.tsx
@@ -13,51 +13,42 @@ const HeroSection = () => {
         </h2>
 
         <h1 className="mt-10 text-3xl font-bold sm:text-4xl">Martin Mwangi</h1>
-        <h1 className="mb-10 mt-5 text-xl font-bold sm:text-2xl opacity-75">
+        <h1 className="mb-6 mt-5 text-xl font-bold sm:text-2xl opacity-75">
           Junior Software Developer
         </h1>
-        <p className="mt-8 max-w-[700px] text-">
+        <p className="mt-5 max-w-[700px] text-">
           With some years of experience in software development, I am dedicated
           to designing and developing User Interfaces and experiences focused on
           their emotions ( UI / UX ), used in web platforms and apps.
         </p>
+        <div className="my-8 flex gap-5">
+          <Link href="https://www.linkedin.com/in/marville001/">
+            <a
+              target="_blank"
+              className="flex cursor-pointer items-center rounded-md bg-primary px-2 py-1 text-white"
+            >
+              <FaLinkedinIn className="pr-2 text-2xl" />
+              <div className="h-4 w-[1px] bg-gray-300"></div>
+              <span className="pl-2 text-lg">LinkedIn</span>
+            </a>
+          </Link>
+          <Link href="https://github.com/marville001">
+            <a
+              target="_blank"
+              className="flex cursor-pointer items-center rounded-md bg-primary px-2 py-1 text-white"
+            >
+              <FaGithubAlt className="pr-2 text-2xl" />
+              <div className="h-4 w-[1px] bg-gray-300"></div>
+              <span className="pl-2 text-lg">Github</span>
+            </a>
+          </Link>
+        </div>
       </div>
       <div
-        style={{
-          backgroundImage: `url('')`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-        }}
-        className="h-40 md:h-auto flex items-center justify-center"
+        className="h-60 md:h-auto flex items-center justify-center"
       >
         <img className='h-[100%] md:h-[80%]' src="/assets/undraw_programming_re_kg9v.svg" alt="Martin Mwangi" />
-        <div className="flex hidden h-full w-full flex-col justify-center px-4 sm:px-10 md:items-end">
-          <p className="bg-orange-400 py-4 px-6 text-xl text-white sm:px-16 sm:text-2xl">
-            Martin Mwangi
-          </p>
-          <div className="my-8 flex gap-5">
-            <Link href="https://www.linkedin.com/in/marville001/">
-              <a
-                target="_blank"
-                className="flex cursor-pointer items-center rounded-md bg-primary px-2 py-1 text-white"
-              >
-                <FaLinkedinIn className="pr-2 text-2xl" />
-                <div className="h-4 w-[1px] bg-gray-300"></div>
-                <span className="pl-2 text-lg">LinkedIn</span>
-              </a>
-            </Link>
-            <Link href="https://github.com/marville001">
-              <a
-                target="_blank"
-                className="flex cursor-pointer items-center rounded-md bg-primary px-2 py-1 text-white"
-              >
-                <FaGithubAlt className="pr-2 text-2xl" />
-                <div className="h-4 w-[1px] bg-gray-300"></div>
-                <span className="pl-2 text-lg">Github</span>
-              </a>
-            </Link>
-          </div>
-        </div>
+
       </div>
     </div>
   )
