a6362ce1d5e971a5c0e155a2200e07a320f11e2c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-09-26 21:39:38 +0300

    Updates

--- pages/book-notes/index.tsx
+++ pages/book-notes/index.tsx
@@ -41,7 +41,7 @@ const BookNotes = (props: Props) => {
           <div className="grid gap-12 sm:grid-cols-2">
             {bookNotes?.map((bookNote) => (
               <Link key={bookNote?.id} href="">
-                <a className="flex gap-4 hover:bg-gray-50 dark:hover:bg-dim-dark p-1 rounded-md">
+                <a className="flex gap-4 dark:bg-dim-dark hover:bg-gray-50 dark:hover:bg-dim-dark p-2 rounded-md">
                   <div className="min-w-[100px] sm:min-w-[150px]">
                     <img
                       src={bookNote?.image}
